beb682f9fb3d11aacaf6284bf7d6d4f30a517088
kaito.kojima <kaito.kojima@theport.jp>
2024-01-24 01:59:03 +0900

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root "hello#index" # "hello"はコントローラ名、"index"はそのコントローラのアクション名です。
+
+  
+end
